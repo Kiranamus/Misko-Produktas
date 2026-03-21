cfedeed873bae37d802031e50cd39604ff1623ec
Kiranamus <kastautas8@gmail.com>
2026-03-21 15:03:49 +0200

    more edits
    edits

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,24 @@
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./Home.css";
+
 export default function Home() {
-  return <div className="content"></div>; // empty on purpose
+  const navigate = useNavigate();
+  
+  return (
+    <div className="home">
+      <div className="parag">
+        <p>
+          Programa skirta surasti geriausią miško plotą investavimui
+        </p>
+      </div>
+
+
+      <button className="go-map-btn"
+        onClick={() => navigate("/map")}
+      >
+        Eiti į žemėlapį
+      </button>
+    </div>
+  );
 }
